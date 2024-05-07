8c9d898aecc7b287e2fa56de937f277ee790bc2a
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2024-05-07 22:48:40 +0200

    Add fonts

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,16 @@
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['Barlow', ...defaultTheme.fontFamily.sans],
+				serif: ['Bellefair', ...defaultTheme.fontFamily.serif],
+			},
+		},
 	},
 	plugins: [],
 }
