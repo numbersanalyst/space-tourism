64eac0a07725fdbcfdd88fe09d2baa8beb3cb7f1
numbersanalyst <mrgregorybrzeczyszczykiewicz@proton.me>
2024-05-13 09:50:31 +0200

    Create home page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,10 +12,13 @@ export default {
 				serif: ['Bellefair', ...defaultTheme.fontFamily.serif],
 			},
 			colors: {
-				'main': '#0B0D17',
-				'accent': '#D0D6F9',
-				'white': '#FFFFFF',
+				main: '#0B0D17',
+				accent: '#D0D6F9',
+				white: '#FFFFFF',
 			},
+			boxShadow: {
+				'circle': '0 0 0 70px rgba(255, 255, 255, 0.15)',
+			}
 		},
 	},
 	plugins: [],
